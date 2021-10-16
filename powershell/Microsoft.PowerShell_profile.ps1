5af9d4944bee40c3ebc7df29e14d4abef81a702c
Tanmay Gupta <tanmaygupta2198@gmail.com>
2021-10-16 18:27:03 +0530

    added ipython notebook css file

--- powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-# C:\Users\admin\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
-Set-PSReadLineOption -Colors @{ Command = 'Red' } | Add-Content $Profile
-Invoke-Expression (&starship init powershell)
