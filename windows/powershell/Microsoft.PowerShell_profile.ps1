35c638a87a57fb5eb03ab702f21770c852321549
tanmay <tanmaygupta2198@gmail.com>
2022-10-10 01:43:48 +0530

    added alias for yt-dlp, restart and shutdown

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,22 @@
-# C:\Users\admin\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-
 Set-PSReadLineOption -Colors @{ Command = 'Red' } | Add-Content $Profile
-Invoke-Expression (&starship init powershell)
+
+# initialize starship
+try { Invoke-Expression (&starship init powershell) } catch {}
+
+# yt-dlp
+function yt([string]$URL) {
+    yt-dlp --add-chapters --write-subs --write-auto-subs --sub-langs en* --embed-subs --compat-options no-keep-subs --sponsorblock-mark all $URL
+}
+function yt1080([string]$URL) {
+    yt-dlp -S "res:1080,fps" --add-chapters --write-subs --write-auto-subs --sub-langs en* --embed-subs --compat-options no-keep-subs --sponsorblock-mark all $URL
+}
+function yta([string]$URL) {
+    yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 $URL
+}
+
+# find
+New-Alias -Name "find" -Value "where.exe"
+# shutdown
+New-Alias -Name "sd" -Value "Stop-Computer -ComputerName localhost -Confirm -Force"
+# restart
+New-Alias -Name "rs" -Value "Restart-Computer -Confirm -Force"
